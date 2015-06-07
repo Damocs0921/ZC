d24ded91e17328e00c0551acdf498d07f28a1ff3
DamocsYang <damocsyang@DamocsYangs-MacBook-Pro.local>
2015-06-07 14:40:55 +0800

    Initial Commit

---
+++ ZC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZC
+//
+//  Created by DamocsYang on 6/7/15.
+//  Copyright (c) 2015 DamocsYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZC
+//
+//  Created by DamocsYang on 6/7/15.
+//  Copyright (c) 2015 DamocsYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
